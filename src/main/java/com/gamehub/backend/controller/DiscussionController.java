a34474cc484ed00c9620ecef30398ce8aecb4a7d
zijian yi <zijianyi66@gmail.com>
2023-11-26 20:56:15 -0600

    change crossorigin

--- src/main/java/com/gamehub/backend/controller/DiscussionController.java
+++ src/main/java/com/gamehub/backend/controller/DiscussionController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/discussions")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class DiscussionController {
 
     private final DiscussionService discussionService;
